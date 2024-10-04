5a608f26bf908be09e2169b2154ea2a67651b731
PROMISE811 <chiapromise3@gmail.com>
2024-10-04 12:22:17 +0100

    Add Render host to ALLOWED_HOSTS

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x3g31a1%i%x*^x)dexx424#)ed&q1=uj%($ce0=3cbmw=uzvk%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'promise-3.onrender.com']
 
 
 # Application definition
